f8333222b2b89bbd8b0564b25e2b2787f3505530
NuclearMystic <solarcompost@gmail.com>
2024-07-27 19:39:57 -0400

    Initial commit

---
+++ Data/InventoryContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class InventoryContext : DbContext
+{
+    public InventoryContext(DbContextOptions<InventoryContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<InventoryItem> InventoryItems { get; set; }
+    // Define other DbSets as needed
+}
---
+++ Models/InventoryItem.cs
@@ -0,0 +1,8 @@
+﻿public class InventoryItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public int ParLevel { get; set; }
+    // Additional properties...
+}
